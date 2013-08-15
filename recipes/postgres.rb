f7f9a8f3a44e9e55784856a9950d33cd104bf39f
Nicolas Ledez <github@ledez.net>
2013-08-15 13:42:55 +0200

    Add munin-plugin-postgres

---
+++ recipes/postgres.rb
@@ -0,0 +1,94 @@
+#
+# Cookbook Name:: rehost-munin
+# Recipe:: postgres
+#
+# Copyright 2013, ReHost
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "rehost-munin"
+
+#node['rehost-munin']['packages-postgres'].each do |pkg|
+#  package pkg do
+#    action :install
+#  end
+#end
+
+link "/etc/munin/plugins/postgres_bgwriter" do
+  to "/usr/share/munin/plugins/postgres_bgwriter"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_cache_ALL" do
+  to "/usr/share/munin/plugins/postgres_cache_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_checkpoints" do
+  to "/usr/share/munin/plugins/postgres_checkpoints"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_connections_ALL" do
+  to "/usr/share/munin/plugins/postgres_connections_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_connections_db" do
+  to "/usr/share/munin/plugins/postgres_connections_db"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_locks_ALL" do
+  to "/usr/share/munin/plugins/postgres_locks_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_querylength_ALL" do
+  to "/usr/share/munin/plugins/postgres_querylength_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_scans_ALL" do
+  to "/usr/share/munin/plugins/postgres_scans_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_size_ALL" do
+  to "/usr/share/munin/plugins/postgres_size_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_transactions_ALL" do
+  to "/usr/share/munin/plugins/postgres_transactions_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_tuples_ALL" do
+  to "/usr/share/munin/plugins/postgres_tuples_"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_users" do
+  to "/usr/share/munin/plugins/postgres_users"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
+
+link "/etc/munin/plugins/postgres_xlog" do
+  to "/usr/share/munin/plugins/postgres_xlog"
+  notifies :restart, "service[#{node['rehost-munin']['munin-node']}]"
+  action :create
+end
---
+++ spec/postgres_spec.rb
@@ -0,0 +1,66 @@
+require 'chefspec'
+
+describe 'rehost-munin::postgres' do
+  it 'should deploy a munin postgres' do
+    chef_run = ChefSpec::ChefRunner.new
+    chef_run.converge 'rehost-munin::postgres'
+
+    #runner = expect(chef_run)
+
+    #[ "liblwp-useragent-determined-perl" ].each do |p|
+      #runner.to install_package p
+    #end
+
+    link = chef_run.link("/etc/munin/plugins/postgres_bgwriter")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_bgwriter"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_cache_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_cache_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_checkpoints")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_checkpoints"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_connections_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_connections_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_connections_db")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_connections_db"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_locks_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_locks_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_querylength_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_querylength_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_scans_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_scans_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_size_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_size_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_transactions_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_transactions_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_tuples_ALL")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_tuples_"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_users")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_users"
+    expect(link).to notify "service[munin-node]", :restart
+
+    link = chef_run.link("/etc/munin/plugins/postgres_xlog")
+    expect(link).to link_to "/usr/share/munin/plugins/postgres_xlog"
+    expect(link).to notify "service[munin-node]", :restart
+  end
+end
