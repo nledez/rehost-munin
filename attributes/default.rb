ad501afb6cc4a54a1f6cc55769b663d9b7424b2c
Nicolas Ledez <github@ledez.net>
2013-09-19 20:14:27 +0200

    Change Array format

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,10 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default['rehost-munin']['packages'] = [ "munin-node" ]
-default['rehost-munin']['packages-master'] = [ "munin", "nginx", "apache2-utils" ]
-default['rehost-munin']['packages-nginx'] = [ "liblwp-useragent-determined-perl" ]
-default['rehost-munin']['packages-postgres'] = [ "libdbd-pg-perl" ]
+default['rehost-munin']['packages'] = %w{munin-node}
+default['rehost-munin']['packages-master'] = %w{munin nginx apache2-utils}
+default['rehost-munin']['packages-nginx'] = %w{liblwp-useragent-determined-perl}
+default['rehost-munin']['packages-postgres'] = %w{libdbd-pg-perl}
 default['rehost-munin']['munin-node'] = "munin-node"
 default['rehost-munin']['allowed_host'] = "127.0.0.1"
 default['rehost-munin']['vhost'] = "munin.localhost"
