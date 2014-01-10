20545c79addb9534be0f95b4eed493d2d38e1ed6
Nicolas Ledez <github@ledez.net>
2014-01-10 23:15:59 +0100

    Fix maintainer & Licence. But real opensource one

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'rehost-munin'
-maintainer       'ReHost'
-maintainer_email 'nicolas@ledez.net'
-license          'All rights reserved'
+maintainer       'ReHost - Nicolas Ledez'
+maintainer_email 'chef-cookbook@ledez.net'
+license          'Apache v2.0'
 description      'Installs/Configures rehost-munin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.13'
